c78718bfa3f04dfaf20aaa8e0af744ea82826bd2
Augustine Salas <107865800+AugustineSalas@users.noreply.github.com>
2024-03-28 12:31:56 +0530

    2

--- Dictionary/billing.py
+++ Dictionary/billing.py
@@ -17,11 +17,12 @@ def display_stock():
 
 def calculate_bill(items, number):
     total_bill = 0
+    i=0
     for item in items:
-        for i in range(number):
-            if item in stock and stock[item] > 0:
-                total_bill += price[item]
-                stock[item] -= 1
+        if item in stock and stock[item] > 0:
+            total_bill += number[i]*price[item]
+            stock[item] -= number[i]
+            i+=1
     return total_bill
 
 def display_remaining_stock():
@@ -30,13 +31,15 @@ def display_remaining_stock():
         print(f"{item}: {stock[item]}")
 
 display_stock()
+no_items1 = []
 while True:
     items_to_buy = input("Enter the items you want to buy (separated by commas): ").split(",")
 
     no_items = input("Enter the no of items you want to buy (separated by commas): ").split(",")
-    no_items1 = int(i) for i in no_items
+    for i in no_items:
+        no_items1.append(int(i))
 
-    total_bill = calculate_bill(items_to_buy, no_items)
+    total_bill = calculate_bill(items_to_buy, no_items1)
     print(f"Total bill: ${total_bill}")
 
     display_remaining_stock()
